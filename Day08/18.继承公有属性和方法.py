ca61bf3903d8cd1fbf088d4219827b71c54c2c2f
codeBeefFly <lijin890119@163.com>
2020-09-28 21:26:50 +0800

    类对象不能访问类的私有成员，子类+对象亦不能，但可以通过调用父类方法访问

--- Day08/18.继承公有属性和方法.py
+++ Day08/18.继承公有属性和方法.py
@@ -11,11 +11,19 @@ class Father:
 class Son(Father):
     def haha(self):
         # 访问父类的私有属性和私有方法
-        # print(self.__age)
-        self.__func()
+        # print(self.__age)  # 父类私有成员+函数不能被子类访问 __age
+        # self.__func()  # 父类私有成员+函数不能被子类访问  __func
+        pass
+
+
+father = Father()
+father.sayHello()
+# 私有成员+函数不能被对象访问
+# father.__func()  # unresolved attribute reference __func for class Father
 
 son = Son()
 # print(son.name)
 # # print(son.__age)
 # son.sayHello()
+son.sayHello()  # 子类可以通过调用继承的父类的方法访问父类的私有成员
 son.haha()
